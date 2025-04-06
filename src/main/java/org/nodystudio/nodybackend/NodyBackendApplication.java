a805e96ac9003bc134bebee6631cfc2805ea7c18
ByeongGyu An <an622911@gmail.com>
2025-04-06 15:55:40 +0900

    Initialize Nody backend

---
+++ src/main/java/org/nodystudio/nodybackend/NodyBackendApplication.java
@@ -0,0 +1,13 @@
+package org.nodystudio.nodybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nodystudio/nodybackend/NodyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nodystudio.nodybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
